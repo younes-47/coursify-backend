ed811bf0fd72032ca87eab876e445a207bc3df0c
Younes <101892216+younes-47@users.noreply.github.com>
2024-04-01 20:27:14 +0000

    User repo

---
+++ coursify-backend/Interfaces/IRepository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using coursify_backend.DTO.GET;
+
+namespace coursify_backend.Interfaces.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<UserCredential> GetCredentialByEmailAsync(string email);
+        Task<bool> IsRegistered(string email);
+    }
+}
---
+++ coursify-backend/Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using coursify_backend.DTO.GET;
+using coursify_backend.Interfaces.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace coursify_backend.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly CoursifyContext _context;
+        public UserRepository(CoursifyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserCredential> GetCredentialByEmailAsync(string email)
+        {
+            return await _context.Users
+                .Where(u => u.Email == email)
+                .Select(u => new UserCredential
+                {
+                    Email = u.Email,
+                    Password = u.Password,
+                    Role = u.Role
+                })
+                .FirstAsync();
+        }
+
+        public async Task<bool> IsRegistered(string email)
+        {
+            return await _context.Users.AnyAsync(u => u.Email == email);
+        }
+    }
+}
